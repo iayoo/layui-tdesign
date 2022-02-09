84a0d92e9dd68bc572bd522234861f3f73072b80
vilnius <vilnius94@163.com>
2022-02-09 22:58:07 +0800

    # base 控制器

--- app/admin/controller/BaseController.php
+++ app/admin/controller/BaseController.php
@@ -3,12 +3,9 @@ declare (strict_types = 1);
 
 namespace app\admin\controller;
 
-use Iayoo\ApiResponse\Response\ThinkPHP\ResponseTrait;
-use think\facade\View;
-use think\Request;
+use app\TempBaseController;
 
-class BaseController extends \app\BaseController
+class BaseController extends TempBaseController
 {
-
 
 }
