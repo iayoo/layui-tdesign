0a55b01580920ca844264902bbb9d963c307a0bf
Iayoo <vilnius94@163.com>
2022-02-09 17:47:20 +0800

    # 新增微信扫码登录

---
+++ app/admin/controller/authentication/Wechat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\admin\controller\authentication;
+
+class Wechat extends \app\admin\controller\BaseController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
