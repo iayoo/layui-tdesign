c0df73b7159832f4b491130433ed8000ff27b181
Iayoo <vilnius94@163.com>
2022-02-09 21:06:28 +0800

    # 权限列表及新增权限页面

--- app/admin/controller/administrator/Permission.php
+++ app/admin/controller/administrator/Permission.php
@@ -3,10 +3,23 @@
 namespace app\admin\controller\administrator;
 
 use app\admin\controller\BaseController;
+use app\services\admin\AdminPermissionService;
+use app\services\ToolService;
 
 class Permission extends BaseController
 {
     public function index(){
         return $this->fetch();
+    }
+
+    public function getList(AdminPermissionService $adminPermissionService){
+        return $this->success('success',$adminPermissionService->all());
+    }
+
+    public function add(AdminPermissionService $adminPermissionService)
+    {
+        return $this->fetch('',[
+            'permissions' => ToolService::getTree($adminPermissionService->all()->toArray())
+        ]);
     }
 }
