cf8ccef36756f6b7479b37f760b19951203d0665
Iayoo <vilnius94@163.com>
2022-02-10 18:39:16 +0800

    # 新获取路由功能

--- app/admin/controller/administrator/Permission.php
+++ app/admin/controller/administrator/Permission.php
@@ -24,24 +24,24 @@ class Permission extends BaseController
     }
 
     public function getPermissionList(){
-        $data = [
-            ['id'=>1,'name'=>'登录','path'=>'admin/login'],
-            ['id'=>2,'name'=>'退出登录','path'=>'admin/logout'],
-            ['id'=>3,'name'=>'修改密码','path'=>'admin/change/pass'],
-            ['id'=>4,'name'=>'删除日志','path'=>'admin/log/del'],
-        ];
-        $key = $this->request->param('key');
-        if (!empty($key)){
-            foreach ($data as $l  => $datum){
-                if (strstr($datum['path'],$key) === false){
-                    unset($data[$l]);
-                }
-            }
-            $data = array_values($data);
-        }
+//        $data = [
+//            ['id'=>1,'name'=>'登录','path'=>'admin/login'],
+//            ['id'=>2,'name'=>'退出登录','path'=>'admin/logout'],
+//            ['id'=>3,'name'=>'修改密码','path'=>'admin/change/pass'],
+//            ['id'=>4,'name'=>'删除日志','path'=>'admin/log/del'],
+//        ];
+//        $key = $this->request->param('key');
+//        if (!empty($key)){
+//            foreach ($data as $l  => $datum){
+//                if (strstr($datum['path'],$key) === false){
+//                    unset($data[$l]);
+//                }
+//            }
+//            $data = array_values($data);
+//        }
+        ToolService::getControllerRoute('admin','',['BaseController']);
 
 
-
-        return $this->success('success',$data);
+//        return $this->success('success',$data);
     }
 }
--- app/services/ToolService.php
+++ app/services/ToolService.php
@@ -31,4 +31,77 @@ class ToolService
         }
         return $arr;
     }
+
+    public static function getControllerRoute($module,$pre = '',$exclude = []){
+        $appPath = root_path() . 'app';
+        $modulePath = $appPath . DIRECTORY_SEPARATOR . $module;
+        $controllerPath = $modulePath . DIRECTORY_SEPARATOR . 'controller';
+
+        $namespace = 'app\\' . $module . '\controller';
+        dump($modulePath);
+        dump($controllerPath);
+        if (!is_dir($modulePath) && !is_dir($controllerPath)){
+            return false;
+        }
+        $fileList = self::scandirFolder($controllerPath);
+        foreach ($fileList as $item){
+            if (is_string($item)){
+                $className = str_replace('.php','',$item);
+                if (in_array($className,$exclude)){
+                    continue;
+                }
+                $className = $namespace . '\\' . $className;
+                dump($className);
+                self::getClassMethods($className);
+            }
+        }
+
+//        foreach ($)
+
+
+
+    }
+
+    public static function getClassMethods($classname)
+    {
+        $ref = new \ReflectionClass($classname);
+//        $consts = $ref->getConstants(); //返回所有常量名和值
+//        echo "----------------consts:---------------" . PHP_EOL;
+//        foreach ($consts as $key => $val)
+//        {
+//            dump("$key : $val" . PHP_EOL);
+//        }
+//        $props = $ref->getDefaultProperties(); //返回类中所有属性
+//        echo "--------------------props:--------------" . PHP_EOL . PHP_EOL;
+//        foreach ($props as $key => $val)
+//        {
+//            dump("$key : $val" . PHP_EOL);   // 属性名和属性值
+//        }
+        $methods = $ref->getMethods();   //返回类中所有方法
+        echo "-----------------methods:---------------" . PHP_EOL . PHP_EOL;
+        foreach ($methods as $method)
+        {
+            dump($method);
+            dump($method->getName());
+        }
+    }
+
+    public static function scandirFolder($path)
+    {
+        $list = [];
+        $temp_list = scandir($path);
+        foreach ($temp_list as $file) {
+            //排除根目录
+            if ($file != ".." && $file != ".") {
+                if (is_dir($path . "/" . $file)) {
+                    //子文件夹，进行递归
+                    $list[$file] = self::scandirFolder($path . "/" . $file);
+                } else {
+                    //根目录下的文件
+                    $list[] = $file;
+                }
+            }
+        }
+        return $list;
+    }
 }
