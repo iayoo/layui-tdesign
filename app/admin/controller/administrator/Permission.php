e97d8287899e5e2694c1e5598c73c8b1cf0f1d45
Iayoo <vilnius94@163.com>
2022-02-10 15:31:48 +0800

    # input 搜索表格提示

--- app/admin/controller/administrator/Permission.php
+++ app/admin/controller/administrator/Permission.php
@@ -22,4 +22,26 @@ class Permission extends BaseController
             'permissions' => ToolService::getTree($adminPermissionService->all()->toArray())
         ]);
     }
+
+    public function getPermissionList(){
+        $data = [
+            ['id'=>1,'name'=>'登录','path'=>'admin/login'],
+            ['id'=>2,'name'=>'退出登录','path'=>'admin/logout'],
+            ['id'=>3,'name'=>'修改密码','path'=>'admin/change/pass'],
+            ['id'=>4,'name'=>'删除日志','path'=>'admin/log/del'],
+        ];
+        $key = $this->request->param('key');
+        if (!empty($key)){
+            foreach ($data as $l  => $datum){
+                if (strstr($datum['path'],$key) === false){
+                    unset($data[$l]);
+                }
+            }
+            $data = array_values($data);
+        }
+
+
+
+        return $this->success('success',$data);
+    }
 }
