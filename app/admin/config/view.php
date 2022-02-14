52a254f28050019085a68223ba2395d077353048
Iayoo <vilnius94@163.com>
2022-02-14 09:53:42 +0800

    # 修改模板静态资源路径配置

--- app/admin/config/view.php
+++ app/admin/config/view.php
@@ -9,9 +9,9 @@ return [
         // 配置文件
         '__LAYUI_CONFIG__'     => '/static/plugin/layui/config.js',
         // layui 样式
-        '__LAYUI_CSS__'        => '/static/plugin/layui/css/layui.css',
+        '__LAYUI_CSS__'        => '/static/plugin/layui/2.7.0.rc5/css/layui.css',
         // layui js 资源
-        '__LAYUI_JS__'         => '/static/plugin/layui/layui.all.js',
+        '__LAYUI_JS__'         => '/static/plugin/layui/2.7.0.rc5/layui.js',
         // admin theme 主体样式
         '__ADMIN_THEME_CSS__'  => '/static/plugin/layui-extend/css/admin.css',
         // font awesome 字体样式
