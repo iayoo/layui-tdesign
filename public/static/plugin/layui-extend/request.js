1b26e933a9e178021cee78ba6d368e81933667cf
Iayoo <vilnius94@163.com>
2022-02-14 13:29:15 +0800

    # request 请求扩展重构

--- public/static/plugin/layui-extend/request.js
+++ public/static/plugin/layui-extend/request.js
@@ -5,9 +5,144 @@ layui.define(['jquery', 'layer','loading'], function (exports) {
     const layer = layui.layer;
     const loading = layui.loading;
 
-    let showLoading = true;
+    const Request = function () {
+        Request.prototype.v = '0.0.1';
+        Request.prototype.config = {
+            url:'',
+            data:{},
+            triggerAfterSuccess:false,
+            success:undefined,
+            error:undefined,
+            // 请求成功后重载表单
+            reload_table:0,
+            // 请求成功后刷新页面
+            reload_page:false,
+            // 提交成功关闭当前窗口
+            is_close:false,
+            type:'POST',
+            // 显示loading
+            loading:true
+        }
+        Request.prototype.post = function (options) {
+            if (typeof options === 'object') {
+                console.log(this.config);
+                $.extend(true,  this.config,options);
+                this.send(this.config.url,"POST",this.config.data,this.config);
+            }
+        }
 
-    const request = {};
+        Request.prototype.get = function (options) {
+            if (typeof options === 'object') {
+                $.extend(true, this.config, options);
+                this.send(this.config.url,"GET",this.config.data,this.config);
+            }
+        }
+
+        Request.prototype.delete = function (options){
+            if (typeof options === 'object') {
+                $.extend(true, this.config, options);
+                this.send(this.config.url,"DELETE",this.config.data,this.config);
+            }
+        }
+
+        Request.prototype.send = function (url,type,data,options) {
+            let params = {},that = this;
+            // 处理请求参数
+            if (undefined !== data && 'object' === (typeof data)) {
+                params = JSON.stringify(data)
+            }
+            $.ajax({
+                url: url,
+                type: type,
+                dataType: "json",
+                data: params,
+                contentType: 'application/json',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                success: function (res) {
+                    loading.clear();
+                    if (res === undefined) {
+                        if (undefined !== options.error) {
+                            return options.error(res);
+                        } else {
+                            layer.msg("网络异常", {
+                                icon: 2,
+                                time: 1000
+                            });
+                            return;
+                        }
+                    }
+                    // 判断code是否为未登录code
+                    // 判断当前是否为iframe子页面
+                    // 触发上级页面跳转登录页面
+                    if (
+                        undefined !== res.code &&
+                        res.code === 40100 &&
+                        parent.location.href !== location.href
+                    ) {
+                        layer.msg("登录已过期，正在跳转至登录页", {
+                            icon: 2,
+                            time: 1000
+                        }, function () {
+                            parent.location.href = location.href;
+                        });
+                        return;
+                    }
+                    // code 为 0 时请求正常
+                    if (undefined !== res.code && res.code === 0) {
+                        if (undefined !== res.message) {
+                            console.log(options)
+                            if (options.loading) {
+                                layer.msg(res.message, {
+                                    icon: 1,
+                                    time: 1000
+                                }, function () {
+                                    onSuccess(options, res)
+                                });
+                            } else {
+                                onSuccess(options, res)
+                            }
+
+                        }
+                    } else {
+                        layer.msg(res.message, {
+                            icon: 2,
+                            time: 1000
+                        }, function () {
+                            if (undefined !== options.error) {
+                                options.error(res);
+                            }
+                        });
+                        return false;
+                    }
+                },
+                error: function (res) {
+                    loading.clear();
+                    if (undefined !== res.responseJSON && undefined !== res.responseJSON.message) {
+                        layer.msg(res.responseJSON.message, {
+                            icon: 2,
+                            time: 1000
+                        });
+                    } else {
+                        layer.msg("网络异常", {
+                            icon: 2,
+                            time: 1000
+                        });
+                    }
+
+                    if (undefined !== options.error) {
+                        options.error(res);
+                    }
+                },
+                complete: function (res) {
+                    loading.clear();
+                }
+            })
+        }
+    };
+
+
 
     function onSuccess(options,res){
         if (undefined !== options.is_close && options.is_close){
@@ -18,7 +153,7 @@ layui.define(['jquery', 'layer','loading'], function (exports) {
             parent.layui.table.reload(options.reload_table);
         }
 
-        if (parent.layui !== undefined && parent.layui.afterRequestSuccess !== undefined && typeof parent.layui.afterRequestSuccess === "function"){
+        if (parent.layui !== undefined && parent.layui.afterRequestSuccess !== undefined && typeof parent.layui.afterRequestSuccess === "function" && options.triggerAfterSuccess){
             parent.layui.afterRequestSuccess();
         }
 
@@ -30,117 +165,7 @@ layui.define(['jquery', 'layer','loading'], function (exports) {
             options.success(res);
         }
     }
-    
-    request.delete = function (options) {
 
-    }
-
-    request.post = function (options) {
-
-        if (options.load !== undefined && options.load === false){
-            showLoading = false;
-        }
-        if (showLoading){
-            // console.log(showLoading)
-            loading.load(1)
-        }
-
-        if (undefined === options.type){
-            options.type = "POST";
-        }
-        let params = {};
-        // 处理请求参数
-        if (undefined !== options.data && 'object' === (typeof options.data)){
-            params = JSON.stringify(options.data)
-        }
-        $.ajax({
-            url:options.url,
-            type:"POST",
-            dataType:"json",
-            data:params,
-            contentType:'application/json',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            success: function(res) {
-                loading.clear();
-                if (res === undefined){
-                    if (undefined !== options.error){
-                        return options.error(res);
-                    }else{
-                        layer.msg("网络异常", {
-                            icon: 2,
-                            time: 1000
-                        });
-                        return;
-                    }
-                }
-
-                // 判断code是否为未登录code
-                // 判断当前是否为iframe子页面
-                // 触发上级页面跳转登录页面
-                if (
-                    undefined !==res.code &&
-                    res.code === 40100 &&
-                    parent.location.href !== location.href
-                ){
-                    layer.msg("登录已过期，正在跳转至登录页", {
-                        icon: 2,
-                        time: 1000
-                    },function () {
-                        parent.location.href = location.href;
-                    });
-                    return;
-                }
-                // code 为 0 时请求正常
-                if (undefined!==res.code && res.code === 0){
-                    if (undefined !== res.message){
-                        if (showLoading){
-                            layer.msg(res.message, {
-                                icon: 1,
-                                time: 1000
-                            },function (){
-                                onSuccess(options,res)
-                            });
-                        }else{
-                            onSuccess(options,res)
-                        }
-
-                    }
-                }else{
-                    layer.msg(res.message, {
-                        icon: 2,
-                        time: 1000
-                    },function () {
-                        if (undefined !== options.error){
-                            options.error(res);
-                        }
-                    });
-                    return false;
-                }
-            },
-            error:function (res) {
-                loading.clear();
-                if (undefined !== res.responseJSON && undefined !== res.responseJSON.message){
-                    layer.msg(res.responseJSON.message, {
-                        icon: 2,
-                        time: 1000
-                    });
-                }else{
-                    layer.msg("网络异常", {
-                        icon: 2,
-                        time: 1000
-                    });
-                }
-
-                if (undefined !== options.error){
-                    options.error(res);
-                }
-            },
-            complete:function (res) {
-                loading.clear();
-            }
-        })
-    }
+    let request = new Request();
     exports('request', request);
 });
