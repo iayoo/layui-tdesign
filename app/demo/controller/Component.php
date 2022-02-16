f8ebdbe10def159729140c22288140e17adde3da
Iayoo <vilnius94@163.com>
2022-02-16 17:42:36 +0800

    # 示例文件视图

--- app/demo/controller/Component.php
+++ app/demo/controller/Component.php
@@ -12,4 +12,20 @@ class Component extends BaseController
     public function notify(){
         return $this->fetch();
     }
+
+    public function select(){
+        return $this->fetch();
+    }
+
+    public function inputTable(){
+        if ($this->request->isAjax()){
+            return $this->success('success',[
+                ['title'=>'测试数据1'],
+                ['title'=>'测试数据1'],
+                ['title'=>'测试数据1'],
+                ['title'=>'测试数据1'],
+            ]);
+        }
+        return $this->fetch();
+    }
 }
--- app/demo/controller/Index.php
+++ app/demo/controller/Index.php
@@ -41,6 +41,8 @@ class Index extends BaseController
             ['id'=>1,'title'=>'后台示例','pid'=>0],
             ['id'=>2,'title'=>'组件','pid'=>1,'href'=>''],
             ['id'=>3,'title'=>'消息通知（notify）','pid'=>2,'href'=>'component/notify'],
+            ['id'=>4,'title'=>'下拉框（select）','pid'=>2,'href'=>'component/select'],
+            ['id'=>5,'title'=>'输入框表格提示（input-table）','pid'=>2,'href'=>'component/inputTable'],
         ];
         return $this->success(ToolService::getTree($menu));
     }
